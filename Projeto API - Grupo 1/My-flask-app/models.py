be7da7830177f9aa2f3f9e63efb658e12bcc3d46
Vinicius Ferreira <87578468+viniciusxdxd@users.noreply.github.com>
2024-10-23 11:59:51 -0300

    Adição de docker-compose

--- Projeto API - Grupo 1/My-flask-app/models.py
+++ Projeto API - Grupo 1/My-flask-app/models.py
@@ -2,12 +2,12 @@ from app import db
 
 class Funcionario(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    nome = db.Column(db.String(50), nullable=False)
-    email = db.Column(db.String(50), unique=True, nullable=False)
-    senha = db.Column(db.String(50), nullable=False)
+    nome = db.Column(db.String(100), nullable=False)
+    email = db.Column(db.String(100), unique=True, nullable=False)
+    senha = db.Column(db.String(100), nullable=False)
 
 class Aluno(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    nome = db.Column(db.String(50), nullable=False)
-    email = db.Column(db.String(50), unique=True, nullable=False)
-    curso = db.Column(db.String(50), default="Generation Brasil - 1")
+    nome = db.Column(db.String(100), nullable=False)
+    email = db.Column(db.String(100), unique=True, nullable=False)
+    curso = db.Column(db.String(100), default="Generation Brasil - 1")
